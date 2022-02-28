bc383706aeb6b04050b7667a5aa0654df97feab1
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-02-28 15:21:22 +0530

    Made builder cache concurrent

--- NeuroSpeech.EntityAccessControl/ServiceBuilder.cs
+++ NeuroSpeech.EntityAccessControl/ServiceBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -8,8 +9,8 @@ namespace NeuroSpeech.EntityAccessControl
 {
     internal static class ServiceBuilder
     {
-        private static Dictionary<Type, Func<IServiceProvider, object>> builders
-            = new Dictionary<Type, Func<IServiceProvider, object>>();
+        private static ConcurrentDictionary<Type, Func<IServiceProvider, object>> builders
+            = new ConcurrentDictionary<Type, Func<IServiceProvider, object>>();
 
         private static MethodInfo getRequiredService =
             typeof(Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions)
@@ -18,33 +19,27 @@ namespace NeuroSpeech.EntityAccessControl
 
         public static object? Build(this IServiceProvider services, Type type)
         {
-            if(!builders.TryGetValue(type, out var f))
-            {
+            var f = builders.GetOrAdd(type, (k) => {
                 var c = type.GetConstructors()
                     .OrderBy(x => x.GetParameters()?.Length ?? 0)
                     .FirstOrDefault();
                 if (c == null)
                 {
-                    f = (s) => throw new ArgumentException($"No public constructor found for type {type.FullName}");
+                    return (s) => throw new ArgumentException($"No public constructor found for type {type.FullName}");
                 }
-                else
+                var p = Expression.Parameter(typeof(IServiceProvider));
+                if (c.GetParameters().Length == 0)
                 {
-                    var p = Expression.Parameter(typeof(IServiceProvider));
-                    if (c.GetParameters().Length == 0)
-                    {
-                        f = Expression.Lambda<Func<IServiceProvider, object>>(Expression.New(c), p).Compile();
-                    } else
-                    {
-
-                        var ps = c.GetParameters()
-                            .Select(x => Expression.Call(getRequiredService.MakeGenericMethod(x.ParameterType), p))
-                            .ToArray();
-
-                        f = Expression.Lambda<Func<IServiceProvider, object>>(Expression.New(c, ps), p).Compile();
-                    }
+                    return Expression.Lambda<Func<IServiceProvider, object>>(Expression.New(c), p).Compile();
                 }
-                builders[type] = f;
-            }
+
+                var ps = c.GetParameters()
+                    .Select(x => Expression.Call(getRequiredService.MakeGenericMethod(x.ParameterType), p))
+                    .ToArray();
+
+                return Expression.Lambda<Func<IServiceProvider, object>>(Expression.New(c, ps), p).Compile();
+
+            });
             return f(services);
         }
     }
