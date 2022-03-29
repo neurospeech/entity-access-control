42f767d4a4360fbcc9148d9cc051aa59669137fb
Akash Kava <39438041+ackava@users.noreply.github.com>
2022-03-29 14:32:40 +0530

    EntityJsonSerializer removed

--- NeuroSpeech.EntityAccessControl/EntityJsonSerializerExtensions.cs
+++ NeuroSpeech.EntityAccessControl/EntityJsonSerializerExtensions.cs
@@ -35,6 +35,17 @@ namespace NeuroSpeech.EntityAccessControl
                 new EntityJsonConverterFactory(this)
             }
         };
+
+        public EntitySerializationSettings()
+        {
+
+        }
+
+        public EntitySerializationSettings(DbContext db)
+        {
+            GetTypeName = (x) => db.Model.FindEntityType(x)?.Name ?? x.FullName!;
+            NamingPolicy = JsonNamingPolicy.CamelCase;
+        }
     }
 
     public class ReferenceEqualityComparer : IEqualityComparer<object>
@@ -52,229 +63,229 @@ namespace NeuroSpeech.EntityAccessControl
         }
     }
 
-    public class EntityJsonSerializer
-    {
-        static readonly string DateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'FFFFFFFZ";
+    //public class EntityJsonSerializer
+    //{
+    //    static readonly string DateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'FFFFFFFZ";
 
-        private readonly Dictionary<object, int> added = new(ReferenceEqualityComparer.Instance);
-        private readonly EntitySerializationSettings settings;
-        private readonly Queue<Action> pending = new();
+    //    private readonly Dictionary<object, int> added = new(ReferenceEqualityComparer.Instance);
+    //    private readonly EntitySerializationSettings settings;
+    //    private readonly Queue<Action> pending = new();
 
-        public EntityJsonSerializer(EntitySerializationSettings? settings)
-        {
-            this.settings = settings ?? new EntitySerializationSettings { 
+    //    public EntityJsonSerializer(EntitySerializationSettings? settings)
+    //    {
+    //        this.settings = settings ?? new EntitySerializationSettings { 
                 
-            };
-        }
+    //        };
+    //    }
 
-        public EntityJsonSerializer(DbContext db)
-        {
-            this.settings = new EntitySerializationSettings {
-                GetTypeName = (x) => db.Model.FindEntityType(x)?.Name ?? x.FullName!,
-                NamingPolicy = JsonNamingPolicy.CamelCase,
-            };
-        }
+    //    public EntityJsonSerializer(DbContext db)
+    //    {
+    //        this.settings = new EntitySerializationSettings {
+    //            GetTypeName = (x) => db.Model.FindEntityType(x)?.Name ?? x.FullName!,
+    //            NamingPolicy = JsonNamingPolicy.CamelCase,
+    //        };
+    //    }
 
-        public JsonObject? Serialize(object? entity)
-        {
-            if (entity == null)
-            {
-                return null;
-            }
-            var r = SerializeToJson(entity);
-            while(pending.TryDequeue(out var a))
-            {
-                a();
-            }
-            return r;
-        }
+    //    public JsonObject? Serialize(object? entity)
+    //    {
+    //        if (entity == null)
+    //        {
+    //            return null;
+    //        }
+    //        var r = SerializeToJson(entity);
+    //        while(pending.TryDequeue(out var a))
+    //        {
+    //            a();
+    //        }
+    //        return r;
+    //    }
 
-        public JsonNode? SerializeList<T>(List<T>? items)
-        {
-            if (items == null)
-            {
-                return null;
-            }
-            var r = new JsonArray(items.Count);
-            foreach (var item in items)
-            {
-                if (item == null)
-                {
-                    r.Add(null);
-                    continue;
-                }
-                r.Add(SerializeToJson(item));
-            }
-            while (pending.TryDequeue(out var a))
-            {
-                a();
-            }
-            return r;
-        }
+    //    public JsonNode? SerializeList<T>(List<T>? items)
+    //    {
+    //        if (items == null)
+    //        {
+    //            return null;
+    //        }
+    //        var r = new JsonArray(items.Count);
+    //        foreach (var item in items)
+    //        {
+    //            if (item == null)
+    //            {
+    //                r.Add(null);
+    //                continue;
+    //            }
+    //            r.Add(SerializeToJson(item));
+    //        }
+    //        while (pending.TryDequeue(out var a))
+    //        {
+    //            a();
+    //        }
+    //        return r;
+    //    }
 
-        private JsonObject? SerializeToJson(object e)
-        {
-            if (added.TryGetValue(e, out var existingIndex))
-            {
-                return new JsonObject() {
-                    { "$id", existingIndex }
-                };
-            }
-            var index = added.Count;
-            added[e] = index;
-            var r = new JsonObject() {
-                { "$id", index }
-            };
-            var et = e.GetType();
-            var d = et.StaticCacheGetOrCreate((et) => settings.GetTypeName?.Invoke(et) ?? et.FullName);
-            var namingPolicy = settings.NamingPolicy ?? JsonNamingPolicy.CamelCase;
-            r["$type"] = d;
-            var properties = et.StaticCacheGetOrCreate((et) =>
-                et.GetProperties()
-                .Where(p => 
-                    !(p.GetIndexParameters()?.Length > 0))
-                .Select(p =>
-                {
-                    var propertyType = Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType;
-                    return new
-                    {
-                        PropertyType = propertyType,
-                        p.Name,
-                        GetValue = (Func<object, object>)p.GetValue!,
-                        PropertyInfo = p,
-                        TypeCode = Type.GetTypeCode(propertyType)
-                    };
-                })
-                .ToList());
-            foreach (var p in properties)
-            {
-                var ignoreCondition = settings.GetIgnoreCondition(p.PropertyInfo);
+    //    private JsonObject? SerializeToJson(object e)
+    //    {
+    //        if (added.TryGetValue(e, out var existingIndex))
+    //        {
+    //            return new JsonObject() {
+    //                { "$id", existingIndex }
+    //            };
+    //        }
+    //        var index = added.Count;
+    //        added[e] = index;
+    //        var r = new JsonObject() {
+    //            { "$id", index }
+    //        };
+    //        var et = e.GetType();
+    //        var d = et.StaticCacheGetOrCreate((et) => settings.GetTypeName?.Invoke(et) ?? et.FullName);
+    //        var namingPolicy = settings.NamingPolicy ?? JsonNamingPolicy.CamelCase;
+    //        r["$type"] = d;
+    //        var properties = et.StaticCacheGetOrCreate((et) =>
+    //            et.GetProperties()
+    //            .Where(p => 
+    //                !(p.GetIndexParameters()?.Length > 0))
+    //            .Select(p =>
+    //            {
+    //                var propertyType = Nullable.GetUnderlyingType(p.PropertyType) ?? p.PropertyType;
+    //                return new
+    //                {
+    //                    PropertyType = propertyType,
+    //                    p.Name,
+    //                    GetValue = (Func<object, object>)p.GetValue!,
+    //                    PropertyInfo = p,
+    //                    TypeCode = Type.GetTypeCode(propertyType)
+    //                };
+    //            })
+    //            .ToList());
+    //        foreach (var p in properties)
+    //        {
+    //            var ignoreCondition = settings.GetIgnoreCondition(p.PropertyInfo);
                 
-                if (ignoreCondition == JsonIgnoreCondition.Always)
-                    continue;
-                var name = namingPolicy.ConvertName(p.Name);
-                var propertyType = p.PropertyType;
-                var v = p.GetValue(e);
-                if (v == null)
-                {
-                    if (ignoreCondition == JsonIgnoreCondition.WhenWritingNull)
-                    {
-                        continue;
-                    }                    
-                    r[name] = null;
-                    continue;
-                }
-                switch (p.TypeCode)
-                {
-                    case TypeCode.Boolean:
-                        r[name] = JsonValue.Create((bool)v);
-                        continue;
-                    case TypeCode.Char:
-                        r[name] = JsonValue.Create((char)v);
-                        continue;
-                    case TypeCode.SByte:
-                        r[name] = JsonValue.Create((sbyte)v);
-                        continue;
-                    case TypeCode.Byte:
-                        r[name] = JsonValue.Create((byte)v);
-                        continue;
-                    case TypeCode.Int16:
-                        r[name] = JsonValue.Create((Int16)v);
-                        continue;
-                    case TypeCode.UInt16:
-                        r[name] = JsonValue.Create((UInt16)v);
-                        continue;
-                    case TypeCode.Int32:
-                        if (propertyType.IsEnum)
-                        {
-                            r[name] = propertyType.GetEnumName(v)!;
-                            continue;
-                        }
-                        r[name] = JsonValue.Create((Int32)v);
-                        continue;
-                    case TypeCode.UInt32:
-                        r[name] = JsonValue.Create((UInt32)v);
-                        continue;
-                    case TypeCode.Int64:
-                        r[name] = JsonValue.Create((Int64)v);
-                        continue;
-                    case TypeCode.UInt64:
-                        r[name] = JsonValue.Create((UInt64)v);
-                        continue;
-                    case TypeCode.Single:
-                        r[name] = JsonValue.Create((Single)v);
-                        continue;
-                    case TypeCode.Double:
-                        r[name] = JsonValue.Create((Double)v);
-                        continue;
-                    case TypeCode.Decimal:
-                        r[name] = JsonValue.Create((Decimal)v);
-                        continue;
-                    case TypeCode.DateTime:
-                        r[name] = JsonValue.Create(((DateTime)v).ToString(DateFormat));
-                        continue;
-                    case TypeCode.String:
-                        r[name] = JsonValue.Create((string)v);
-                        continue;
-                }
-                if (propertyType == typeof(DateTimeOffset))
-                {
-                    r[name] = ((DateTimeOffset)v).UtcDateTime.ToString(DateFormat);
-                    continue;
-                }
-                if (propertyType == typeof(Guid))
-                {
-                    r[name] = ((Guid)v).ToString();
-                    continue;
-                }
-                if (v is JsonNode jn)
-                {
-                    r[name] = jn;
-                    continue;
-                }
-                if (v is Geometry g)
-                {
-                    r[name] = g.ToString();
-                    continue;
-                }
-                if (v is System.Collections.IDictionary vd)
-                {
-                    var jd = new JsonObject();
-                    r[name] = jd;
-                    pending.Enqueue(() => {
-                        var ve = vd.GetEnumerator();
-                        while(ve.MoveNext())
-                        {
-                            if (ve.Value == null) {
-                                jd[ve.Key.ToString()!] = null;
-                                continue;
-                            }
-                            jd[ve.Key.ToString()!] = SerializeToJson(ve.Value);
-                        }
-                    });
-                    continue;
-                }
-                if (v is System.Collections.IEnumerable coll)
-                {
-                    var list = new JsonArray();
-                    r[name] = list;
-                    pending.Enqueue(() =>
-                    {
-                        foreach (var c in coll)
-                        {
-                            if (c != null)
-                            {
-                                var jc = SerializeToJson(c);
-                                list.Add(jc);
-                            }
-                        }
-                    });
-                    continue;
-                }
-                r[name] = SerializeToJson(v);
-            }
-            return r;
-        }
-    }
+    //            if (ignoreCondition == JsonIgnoreCondition.Always)
+    //                continue;
+    //            var name = namingPolicy.ConvertName(p.Name);
+    //            var propertyType = p.PropertyType;
+    //            var v = p.GetValue(e);
+    //            if (v == null)
+    //            {
+    //                if (ignoreCondition == JsonIgnoreCondition.WhenWritingNull)
+    //                {
+    //                    continue;
+    //                }                    
+    //                r[name] = null;
+    //                continue;
+    //            }
+    //            switch (p.TypeCode)
+    //            {
+    //                case TypeCode.Boolean:
+    //                    r[name] = JsonValue.Create((bool)v);
+    //                    continue;
+    //                case TypeCode.Char:
+    //                    r[name] = JsonValue.Create((char)v);
+    //                    continue;
+    //                case TypeCode.SByte:
+    //                    r[name] = JsonValue.Create((sbyte)v);
+    //                    continue;
+    //                case TypeCode.Byte:
+    //                    r[name] = JsonValue.Create((byte)v);
+    //                    continue;
+    //                case TypeCode.Int16:
+    //                    r[name] = JsonValue.Create((Int16)v);
+    //                    continue;
+    //                case TypeCode.UInt16:
+    //                    r[name] = JsonValue.Create((UInt16)v);
+    //                    continue;
+    //                case TypeCode.Int32:
+    //                    if (propertyType.IsEnum)
+    //                    {
+    //                        r[name] = propertyType.GetEnumName(v)!;
+    //                        continue;
+    //                    }
+    //                    r[name] = JsonValue.Create((Int32)v);
+    //                    continue;
+    //                case TypeCode.UInt32:
+    //                    r[name] = JsonValue.Create((UInt32)v);
+    //                    continue;
+    //                case TypeCode.Int64:
+    //                    r[name] = JsonValue.Create((Int64)v);
+    //                    continue;
+    //                case TypeCode.UInt64:
+    //                    r[name] = JsonValue.Create((UInt64)v);
+    //                    continue;
+    //                case TypeCode.Single:
+    //                    r[name] = JsonValue.Create((Single)v);
+    //                    continue;
+    //                case TypeCode.Double:
+    //                    r[name] = JsonValue.Create((Double)v);
+    //                    continue;
+    //                case TypeCode.Decimal:
+    //                    r[name] = JsonValue.Create((Decimal)v);
+    //                    continue;
+    //                case TypeCode.DateTime:
+    //                    r[name] = JsonValue.Create(((DateTime)v).ToString(DateFormat));
+    //                    continue;
+    //                case TypeCode.String:
+    //                    r[name] = JsonValue.Create((string)v);
+    //                    continue;
+    //            }
+    //            if (propertyType == typeof(DateTimeOffset))
+    //            {
+    //                r[name] = ((DateTimeOffset)v).UtcDateTime.ToString(DateFormat);
+    //                continue;
+    //            }
+    //            if (propertyType == typeof(Guid))
+    //            {
+    //                r[name] = ((Guid)v).ToString();
+    //                continue;
+    //            }
+    //            if (v is JsonNode jn)
+    //            {
+    //                r[name] = jn;
+    //                continue;
+    //            }
+    //            if (v is Geometry g)
+    //            {
+    //                r[name] = g.ToString();
+    //                continue;
+    //            }
+    //            if (v is System.Collections.IDictionary vd)
+    //            {
+    //                var jd = new JsonObject();
+    //                r[name] = jd;
+    //                pending.Enqueue(() => {
+    //                    var ve = vd.GetEnumerator();
+    //                    while(ve.MoveNext())
+    //                    {
+    //                        if (ve.Value == null) {
+    //                            jd[ve.Key.ToString()!] = null;
+    //                            continue;
+    //                        }
+    //                        jd[ve.Key.ToString()!] = SerializeToJson(ve.Value);
+    //                    }
+    //                });
+    //                continue;
+    //            }
+    //            if (v is System.Collections.IEnumerable coll)
+    //            {
+    //                var list = new JsonArray();
+    //                r[name] = list;
+    //                pending.Enqueue(() =>
+    //                {
+    //                    foreach (var c in coll)
+    //                    {
+    //                        if (c != null)
+    //                        {
+    //                            var jc = SerializeToJson(c);
+    //                            list.Add(jc);
+    //                        }
+    //                    }
+    //                });
+    //                continue;
+    //            }
+    //            r[name] = SerializeToJson(v);
+    //        }
+    //        return r;
+    //    }
+    //}
 
 }
