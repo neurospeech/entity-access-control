b1f3f7b5bcdece7e716978f99cef0a12c5feffab
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-03-07 16:58:00 +0530

    Generic Invoker improved

--- NeuroSpeech.EntityAccessControl/TypeExtensions.cs
+++ NeuroSpeech.EntityAccessControl/TypeExtensions.cs
@@ -341,59 +341,105 @@ namespace NeuroSpeech.EntityAccessControl.Internal
     //}
     public static class Generic
     {
-        private static ConcurrentDictionary<(Type, Type, MethodInfo), object> cache
+        private static ConcurrentDictionary<(Type type1, Type type2, MethodInfo method), object> cache
             = new ConcurrentDictionary<(Type, Type, MethodInfo), object>();
+
+        private static T CreateTypedDelegate<T>(this MethodInfo method)
+            where T: Delegate
+        {
+            return (T)method.CreateDelegate(typeof(T));
+        }
+
+        private static T TypedGet<T>(
+            (Type, Type, MethodInfo) key,
+            Func<(Type type1, Type type2, MethodInfo method), T> create)
+        {
+            return (T)cache.GetOrAdd(key, (x) => create(x));
+        }
+
         public static T InvokeAs<Target, T>(this Target target, Type type, Func<T> fx)
         {
-            var key = (type, type, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type).CreateDelegate(typeof(Func<Target, T>)));
+            var method = TypedGet(
+                    (type,type,fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1)
+                        .CreateTypedDelegate<Func<Target, T>>());
             return method(target);
         }
 
         public static T InvokeAs<Target, T1, T>(this Target target, Type type, Func<T1, T> fx, T1 p1)
         {
-            var key = (type,type, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type).CreateDelegate(typeof(Func<Target, T1, T>)));
+            var method = TypedGet(
+                    (type, type, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1)
+                        .CreateTypedDelegate<Func<Target, T1, T>>());
             return method(target, p1);
         }
 
         public static T InvokeAs<Target, T1, T2, T>(this Target target, Type type, Func<T1, T2, T> fx, T1 p1, T2 p2)
         {
-            var key = (type, type, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T2, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type).CreateDelegate(typeof(Func<Target, T1, T2, T>)));
+            var method = TypedGet(
+                    (type, type, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1)
+                        .CreateTypedDelegate<Func<Target, T1, T2, T>>());
             return method(target, p1, p2);
         }
         public static T InvokeAs<Target, T1, T2, T3, T>(this Target target, Type type, Func<T1, T2, T3, T> fx, T1 p1, T2 p2, T3 p3)
         {
-            var key = (type, type, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T2, T3, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type).CreateDelegate(typeof(Func<Target, T1, T2, T3, T>)));
+            var method = TypedGet(
+                    (type, type, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1)
+                        .CreateTypedDelegate<Func<Target, T1, T2, T3, T>>());
             return method(target, p1, p2, p3);
         }
 
         public static T InvokeAs<Target, T>(this Target target, Type type, Type type2, Func<T> fx)
         {
-            var key = (type, type2, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type, type2).CreateDelegate(typeof(Func<Target, T>)));
+            var method = TypedGet(
+                    (type, type2, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1, k.type2)
+                        .CreateTypedDelegate<Func<Target, T>>());
             return method(target);
         }
 
         public static T InvokeAs<Target, T1, T>(this Target target, Type type, Type type2, Func<T1, T> fx, T1 p1)
         {
-            var key = (type, type2, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type, type2).CreateDelegate(typeof(Func<Target, T1, T>)));
+            var method = TypedGet(
+                    (type, type2, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1, k.type2)
+                        .CreateTypedDelegate<Func<Target, T1, T>>());
             return method(target, p1);
         }
 
         public static T InvokeAs<Target, T1, T2, T>(this Target target, Type type, Type type2, Func<T1, T2, T> fx, T1 p1, T2 p2)
         {
-            var key = (type, type2, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T2, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type, type2).CreateDelegate(typeof(Func<Target, T1, T2, T>)));
+            var method = TypedGet(
+                    (type, type2, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1, k.type2)
+                        .CreateTypedDelegate<Func<Target, T1, T2, T>>());
             return method(target, p1, p2);
         }
         public static T InvokeAs<Target, T1, T2, T3, T>(this Target target, Type type, Type type2, Func<T1, T2, T3, T> fx, T1 p1, T2 p2, T3 p3)
         {
-            var key = (type, type2, fx.Method.GetGenericMethodDefinition());
-            var method = (Func<Target, T1, T2, T3, T>)cache.GetOrAdd(key, (_) => fx.Method.GetGenericMethodDefinition().MakeGenericMethod(type, type2).CreateDelegate(typeof(Func<Target, T1, T2, T3, T>)));
+            var method = TypedGet(
+                    (type, type2, fx.Method),
+                    (k) => k.method
+                        .GetGenericMethodDefinition()
+                        .MakeGenericMethod(k.type1, k.type2)
+                        .CreateTypedDelegate<Func<Target, T1, T2, T3, T>>());
             return method(target, p1, p2, p3);
         }
     }
