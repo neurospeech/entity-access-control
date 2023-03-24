193306de31605f2ff151ee02aa5b0b5cf72db34c
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-03-24 16:10:38 +0530

    Added Combining Reference and Collections into single query

--- NeuroSpeech.EntityAccessControl/Extensions/EntityEntryExtensions.cs
+++ NeuroSpeech.EntityAccessControl/Extensions/EntityEntryExtensions.cs
@@ -32,18 +32,14 @@ namespace NeuroSpeech.EntityAccessControl
                 this.rs = rs;
             }
 
-            public QueryBuilder<T, TR> With(Func<IQueryable<TR>,IQueryable<TR>> func)
-            {
-                return new QueryBuilder<T, TR> (entry, func(rs) );
-            }
-
             public QueryBuilder<T,Selection<TR2,TR>> JoinReference<TR2>(
-                Expression<Func<T,TR2?>> reference)
+                Expression<Func<T,TR2?>> reference,
+                Func<IQueryable<TR2>, IQueryable<TR2>>? postProcess = null)
                 where TR2 : class
             {
                 var rs = this.rs;
                 return new QueryBuilder<T, Selection<TR2, TR>>(entry,
-                    entry.Reference(reference).Query()
+                    entry.Reference(reference).Query().Process(postProcess)
                     .Select(x => new Selection<TR2, TR> {
                         Left = x
                         , Right = rs.ToList()
@@ -52,12 +48,13 @@ namespace NeuroSpeech.EntityAccessControl
             }
 
             public QueryBuilder<T, Selection<TR2, TR>> JoinCollection<TR2>(
-                Expression<Func<T, IEnumerable<TR2>>> collection)
+                Expression<Func<T, IEnumerable<TR2>>> collection,
+                Func<IQueryable<TR2>, IQueryable<TR2>>? postProcess = null)
                 where TR2 : class
             {
                 var rs = this.rs;
                 return new QueryBuilder<T, Selection<TR2, TR>>(entry,
-                    entry.Collection(collection).Query()
+                    entry.Collection(collection).Query().Process(postProcess)
                     .Select(x => new Selection<TR2, TR>
                     {
                         Left = x
@@ -75,19 +72,40 @@ namespace NeuroSpeech.EntityAccessControl
         }
 
         public static QueryBuilder<T,TR> BuildReferenceQuery<T, TR>(
-            this EntityEntry<T> entry, Expression<Func<T,TR?>> reference)
+            this EntityEntry<T> entry,
+            Expression<Func<T,TR?>> reference,
+            Func<IQueryable<TR>, IQueryable<TR>>? postProcess = null)
             where T : class
             where TR: class
         {
-            return new QueryBuilder<T, TR>(entry, entry.Reference(reference).Query());
+            return new QueryBuilder<T, TR>(entry, 
+                entry.Reference(reference)
+                    .Query()
+                    .Process(postProcess)
+            );
         }
 
         public static QueryBuilder<T, TR> BuildCollectionQuery<T, TR>(
-            this EntityEntry<T> entry, Expression<Func<T, IEnumerable<TR>?>> collection)
+            this EntityEntry<T> entry, Expression<Func<T, IEnumerable<TR>?>> collection,
+            Func<IQueryable<TR>, IQueryable<TR>>? postProcess = null)
             where T : class
             where TR : class
         {
-            return new QueryBuilder<T, TR>(entry, entry.Collection(collection).Query());
+            return new QueryBuilder<T, TR>(entry,
+                entry.Collection(collection)
+                    .Query()
+                    .Process(postProcess));
+        }
+
+        private static IQueryable<T> Process<T>(
+            this IQueryable<T> q, 
+            Func<IQueryable<T>,IQueryable<T>>? fx = null)
+        {
+            if (fx == null)
+            {
+                return q;
+            }
+            return q.Process(fx);
         }
     }
 }
